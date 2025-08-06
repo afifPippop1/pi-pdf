e218c1d79c8d490accd414c69e28bf6849f94551
afifPippop1 <afifpippop@gmail.com>
2025-08-06 13:33:21 +0700

    refactor: 💡 move arrow to component

---
+++ app/components/atoms/PopoverArrow.tsx
@@ -0,0 +1,16 @@
+export function PopoverArrow() {
+  return (
+    <svg
+      className="absolute top-0 left-4 -translate-x-1/2 -translate-y-1/2"
+      width="16"
+      height="8"
+      viewBox="0 0 16 8"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8 0 C8.5 0 9.5 0.5 10 1 L16 8 H0 L6 1 C6.5 0.5 7.5 0 8 0 Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
--- app/components/molecules/ContextMenu.tsx
+++ app/components/molecules/ContextMenu.tsx
@@ -6,6 +6,7 @@ import {
   useState,
   type ReactNode,
 } from "react";
+import { PopoverArrow } from "../atoms/PopoverArrow";
 
 type Primitive = string | number | boolean;
 
@@ -82,19 +83,7 @@ export function ContextMenuContent({ children }: { children: ReactNode }) {
       }
       onClick={() => setShow(false)}
     >
-      <svg
-        className="absolute top-0 left-4 -translate-x-1/2 -translate-y-1/2"
-        width="16"
-        height="8"
-        viewBox="0 0 16 8"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M8 0 C8.5 0 9.5 0.5 10 1 L16 8 H0 L6 1 C6.5 0.5 7.5 0 8 0 Z"
-          fill="white"
-        />
-      </svg>
-
+      <PopoverArrow />
       <div className="p-4 text-sm text-gray-700">{children}</div>
     </div>
   );
