e5d433fb819db96650fb2e2e315ec24b323ef7b2
afifPipop1 <afifpippop@gmail.com>
2025-09-01 20:00:48 +0700

    style: 💄 tool picker active color

--- app/components/pages/HomePage.tsx
+++ app/components/pages/HomePage.tsx
@@ -8,12 +8,19 @@ export function HomePage() {
   const navigate = useNavigate();
   return (
     <div className="lg:max-w-4xl w-full flex flex-col items-center">
-      <h1 className="lg:text-3xl text-2xl font-bold text-gray-800 mb-2">Welcome to Pi-DF</h1>
-      <p className="text-gray-500 text-xs lg:text-base mb-8">Upload and annotate your pdf easily</p>
+      <h1 className="lg:text-3xl text-2xl font-bold text-gray-800 mb-2">
+        Welcome to Pi-DF
+      </h1>
+      <p className="text-gray-500 text-xs lg:text-base mb-8">
+        Upload and annotate your pdf easily
+      </p>
       <Dropzone
         onDrop={async (files) => {
           await setPDFDoc(files);
           navigate("/editor");
+        }}
+        accept={{
+          "application/pdf": [],
         }}
       >
         {({ getRootProps, getInputProps }) => (
@@ -23,7 +30,9 @@ export function HomePage() {
           >
             <input {...getInputProps()} />
             <UploadBoxIcon color="transparent" />
-            <p className="text-xs md:text-base text-gray-500">Start by dropping your file's here</p>
+            <p className="text-xs md:text-base text-gray-500">
+              Start by dropping your file's here
+            </p>
             <p className="text-xs md:text-sm text-gray-500">or</p>
             <Button>Select from device</Button>
           </div>
