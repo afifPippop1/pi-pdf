0728d31b484908784a5e52b58f364a7d80371341
afifPipop1 <afifpippop@gmail.com>
2025-09-15 23:13:15 +0700

    style: 💄 revamp color picker

--- app/components/molecules/Popover.tsx
+++ app/components/molecules/Popover.tsx
@@ -7,19 +7,37 @@ import {
   type ReactNode,
 } from "react";
 import { PopoverArrow } from "~/components/atoms/PopoverArrow";
+import { useFloating, offset, flip, shift } from "@floating-ui/react";
 
 interface PopoverProps {
   children: ReactNode;
 }
 
+import type { ExtendedRefs, ReferenceType } from "@floating-ui/react";
+
 interface IPopoverContext {
   show: boolean;
   setShow: (show: boolean) => void;
+  x: number | null;
+  y: number | null;
+  refs: ExtendedRefs<ReferenceType>;
+  strategy: "absolute" | "fixed";
 }
 
 const PopoverContext = createContext<IPopoverContext>({
   show: false,
   setShow() {},
+  x: null,
+  y: null,
+  refs: {
+    reference: { current: null },
+    floating: { current: null },
+    domReference: { current: null },
+    setFloating: () => {},
+    setPositionReference: () => {},
+    setReference: () => {},
+  },
+  strategy: "absolute",
 });
 
 function usePopover() {
@@ -29,6 +47,11 @@ function usePopover() {
 export default function Popover(props: PopoverProps) {
   const contextRef = useRef<HTMLDivElement>(null);
   const [show, setShow] = useState(false);
+
+  const { x, y, refs, strategy } = useFloating({
+    placement: "bottom-start", // default placement
+    middleware: [offset(8), flip(), shift({ padding: 8 })],
+  });
 
   useEffect(() => {
     function handleClickOutside(event: MouseEvent | Event) {
@@ -47,7 +70,7 @@ export default function Popover(props: PopoverProps) {
   }, []);
 
   return (
-    <PopoverContext.Provider value={{ show, setShow }}>
+    <PopoverContext.Provider value={{ show, setShow, x, y, refs, strategy }}>
       <div className="relative inline-block" ref={contextRef}>
         {props.children}
       </div>
@@ -56,13 +79,15 @@ export default function Popover(props: PopoverProps) {
 }
 
 export function PopoverTrigger({ children }: { children: ReactNode }) {
-  const { setShow } = usePopover();
+  const { setShow, refs } = usePopover();
+
   const handleClick = (e: React.MouseEvent) => {
     e.preventDefault();
     setShow(true);
   };
   return (
     <div
+      ref={refs.setReference}
       onClick={handleClick}
       className="flex items-center justify-center cursor-pointer select-none h-full"
     >
@@ -72,20 +97,22 @@ export function PopoverTrigger({ children }: { children: ReactNode }) {
 }
 
 export function PopoverContent({ children }: { children: ReactNode }) {
-  const { show } = usePopover();
-  return (
+  const { show, x, y, refs, strategy } = usePopover();
+
+  return show ? (
     <div
-      id="popoverPanel"
-      className={
-        show
-          ? "absolute z-10 mt-2 min-w-48 rounded-md shadow-xl bg-gray-50"
-          : "hidden"
-      }
+      ref={refs.setFloating}
+      style={{
+        position: strategy,
+        top: y ?? 0,
+        left: x ?? 0,
+      }}
+      className="z-10 min-w-48 rounded-md shadow-xl bg-gray-50"
     >
       <PopoverArrow fill="#f9fafb" />
       <div className="p-4 text-sm text-gray-700">{children}</div>
     </div>
-  );
+  ) : null;
 }
 
 export function PopoverItem({
--- app/components/molecules/Tools/ColorPicker.tsx
+++ app/components/molecules/Tools/ColorPicker.tsx
@@ -1,24 +1,68 @@
 import { RgbaColorPicker, type RgbaColor } from "react-colorful";
 import type { Color } from "~/types";
+import { rgbToString } from "~/utils";
+import Popover, { PopoverContent, PopoverTrigger } from "../Popover";
 
-const defaultColor = {
-  a: 1,
-  r: 255,
-  g: 255,
-  b: 255,
-} as Color;
+const whiteColor = rgbToString({ r: 255, g: 255, b: 255, a: 1 });
+const whiteColorTransparent = rgbToString({ r: 255, g: 255, b: 255, a: 0 });
+
+const defaultColorOptions: Color[] = [
+  { r: 30, g: 30, b: 30, a: 1 },
+  { r: 224, g: 49, b: 49, a: 1 },
+  { r: 47, g: 158, b: 68, a: 1 },
+  { r: 25, g: 113, b: 194, a: 1 },
+  { r: 240, g: 140, b: 0, a: 1 },
+];
 
 interface ColorPickerProps {
-  color?: Color;
+  color: Color;
   onChange: (color: RgbaColor) => void;
   title?: string;
 }
 
 export function ColorPicker({ color, onChange, title }: ColorPickerProps) {
   return (
-    <div className="flex flex-col gap-2">
-      {!!title && <p className="text-sm">{title}</p>}
-      <RgbaColorPicker color={color || defaultColor} onChange={onChange} />
+    <div className="flex gap-2 items-center px-2">
+      {defaultColorOptions.map((color, index) => (
+        <ColorItem color={color} key={index} onClick={onChange} />
+      ))}
+      <span className="text-gray-300">|</span>
+      <Popover>
+        <PopoverTrigger>
+          <ColorItem color={color} custom />
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="flex flex-col gap-2 text-xs">
+            <p>Colors</p>
+            <RgbaColorPicker color={color} onChange={onChange} />
+          </div>
+        </PopoverContent>
+      </Popover>
     </div>
+  );
+}
+
+function ColorItem({
+  color,
+  custom,
+  onClick,
+}: {
+  color: Color;
+  custom?: boolean;
+  onClick?: (color: Color) => void;
+}) {
+  const backgroundColor = rgbToString(color);
+  return (
+    <div
+      onClick={() => onClick?.(color)}
+      className={`${!custom ? "w-6 h-6" : "w-7 h-7"} ${
+        !custom && "transition-transform hover:scale-125"
+      } rounded cursor-pointer ${
+        (backgroundColor === whiteColor ||
+          backgroundColor === whiteColorTransparent) &&
+        "border border-gray-300"
+      }`}
+      style={{ backgroundColor }}
+    ></div>
   );
 }
