11691beecc0b815309de007f0720cd944ff1f4ca
afifPipop1 <afifpippop@gmail.com>
2025-09-19 15:33:07 +0700

    fix: 🐛 textarea position

--- app/components/molecules/TextEditor.tsx
+++ app/components/molecules/TextEditor.tsx
@@ -26,7 +26,7 @@ export function TextEditor({ action, onBlur }: TextEditorProps) {
     }
     return toolState.TEXT.fontSize;
   }, [selectedElement, toolState]);
-  const verticalOffset = useMemo(() => -0.3 * fontSize, [fontSize]);
+  const verticalOffset = useMemo(() => -0.15 * fontSize, [fontSize]);
 
   useEffect(() => {
     if (action === actions.WRITING) {
@@ -49,14 +49,15 @@ export function TextEditor({ action, onBlur }: TextEditorProps) {
         margin: 0,
         padding: 0,
         border: 0,
-        outline: 0,
+        outline: "none",
         overflow: "hidden",
         whiteSpace: "pre",
         background: "transparent",
         resize: "none",
         fontSize: fontSize * zoom,
         fontFamily,
-        lineHeight: 1,
+        letterSpacing: 1,
+        lineHeight: 1.2,
       }}
       onBlur={onBlur}
       // @ts-ignore
